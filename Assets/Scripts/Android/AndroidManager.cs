944d6a8ded5642c8f09f7abccf5b7c7785395cd6
Evgeniy Mineev <e.mineev@crazypanda.ru>
2024-05-19 11:27:10 +0400

    use MANAGE_EXTERNAL_STORAGE permission for access files for Android version > 11

--- Assets/Scripts/Android/AndroidManager.cs
+++ Assets/Scripts/Android/AndroidManager.cs
@@ -8,9 +8,58 @@ namespace DaggerfallWorkshop
     {
         private void Start()
         {
+            RequestManageAllFilesAccess();
+
             CopyStreamingAssetsToInternalMemory();
 
             SceneManager.LoadScene(1);
+        }
+
+        //https://stackoverflow.com/a/76256946
+        private void RequestManageAllFilesAccess()
+        {
+            using (var buildVersion = new AndroidJavaClass("android.os.Build$VERSION"))
+            {
+                using (var buildCodes = new AndroidJavaClass("android.os.Build$VERSION_CODES"))
+                {
+//Check SDK version > 29
+                    if (buildVersion.GetStatic<int>("SDK_INT") > buildCodes.GetStatic<int>("Q"))
+                    {
+                        using (var environment = new AndroidJavaClass("android.os.Environment"))
+                        {
+                            //сhecking if permission already exists
+                            if (!environment.CallStatic<bool>("isExternalStorageManager"))
+                            {
+                                using (var settings = new AndroidJavaClass("android.provider.Settings"))
+                                {
+                                    using (var uri = new AndroidJavaClass("android.net.Uri"))
+                                    {
+                                        using (var unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+                                        {
+                                            using (var currentActivity =
+                                                   unityPlayer.GetStatic<AndroidJavaObject>("currentActivity"))
+                                            {
+                                                using (var parsedUri =
+                                                       uri.CallStatic<AndroidJavaObject>("parse",
+                                                           $"package:{Application.identifier}"))
+                                                {
+                                                    using (var intent = new AndroidJavaObject("android.content.Intent",
+                                                               settings.GetStatic<string>(
+                                                                   "ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION"),
+                                                               parsedUri))
+                                                    {
+                                                        currentActivity.Call("startActivity", intent);
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
 
         private void CopyStreamingAssetsToInternalMemory()
