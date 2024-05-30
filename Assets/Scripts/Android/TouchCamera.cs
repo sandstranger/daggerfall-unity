b4693667cb0b59a814dbba8fd52190129d746f98
Evgeniy Mineev <e.mineev@crazypanda.ru>
2024-05-30 21:36:38 +0400

    simplify touch camera code

--- Assets/Scripts/Android/TouchCamera.cs
+++ Assets/Scripts/Android/TouchCamera.cs
@@ -5,12 +5,9 @@ namespace DaggerfallWorkshop.Game
 {
     sealed class TouchCamera : EventTrigger
     {
-        public static Vector2 CurrentTouchPosition { get; private set; }
+        public static Vector2 CurrentTouchDelta { get; private set; }
 
         private const float Smoothness = 5.0f;
-        private const float TouchDeadZone = 0.1f;
-
-        private Vector2 _previousTouchPosition;
 
         public override void OnDrag(PointerEventData data)
         {
@@ -19,13 +16,7 @@ namespace DaggerfallWorkshop.Game
                 return;
             }
 
-            var delta = data.delta / Smoothness;
-
-            if ( Mathf.Abs(_previousTouchPosition.x - delta.x) > TouchDeadZone ||
-                 Mathf.Abs(_previousTouchPosition.y - delta.y) > TouchDeadZone )
-            {
-                CurrentTouchPosition = _previousTouchPosition = delta;
-            }
+            CurrentTouchDelta = data.delta / Smoothness;
         }
 
         public override void OnPointerDown(PointerEventData data)
@@ -35,22 +26,20 @@ namespace DaggerfallWorkshop.Game
                 return;
             }
 
-            CurrentTouchPosition = _previousTouchPosition = data.delta / Smoothness;
+            CurrentTouchDelta = data.delta / Smoothness;
         }
 
         public override void OnPointerUp(PointerEventData data)
         {
-            PointerEventData pointerEventData = (PointerEventData) data;
-
-            if (pointerEventData.pointerId <= 0)
+            if (data.pointerId <= 0)
             {
-                CurrentTouchPosition = _previousTouchPosition = Vector2.zero;
+                CurrentTouchDelta = Vector2.zero;
             }
         }
 
         private void LateUpdate()
         {
-            CurrentTouchPosition = Vector2.zero;
+            CurrentTouchDelta = Vector2.zero;
         }
     }
 }
