bb2352fde98da092eaabfd0536f03365457dab95
Evgeniy Mineev <e.mineev@crazypanda.ru>
2024-06-02 00:07:08 +0400

    use interfaces for dragging in touch camera

--- Assets/Scripts/Android/ScreenControls/TouchCamera.cs
+++ Assets/Scripts/Android/ScreenControls/TouchCamera.cs
@@ -3,14 +3,14 @@ using UnityEngine.EventSystems;
 
 namespace DaggerfallWorkshop.Game
 {
-    sealed class TouchCamera : EventTrigger
+    sealed class TouchCamera : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
     {
         public Vector2 CurrentTouchDelta { get; private set; }
 
         private const float Smoothness = 5.0f;
         private int _pointerId = -1;
 
-        public override void OnDrag(PointerEventData data)
+        public void OnDrag(PointerEventData data)
         {
             if (data.pointerId == _pointerId)
             {
@@ -18,7 +18,7 @@ namespace DaggerfallWorkshop.Game
             }
         }
 
-        public override void OnPointerDown(PointerEventData data)
+        public void OnPointerDown(PointerEventData data)
         {
             if (_pointerId >= 0)
             {
@@ -29,7 +29,7 @@ namespace DaggerfallWorkshop.Game
             CurrentTouchDelta = data.delta / Smoothness;
         }
 
-        public override void OnPointerUp(PointerEventData data)
+        public void OnPointerUp(PointerEventData data)
         {
             if (data.pointerId == _pointerId)
             {
