09e1c5ea9af32e7cc729d9091e3bbe61f3e47a64
Evgeniy Mineev <e.mineev@crazypanda.ru>
2024-07-06 18:01:38 +0400

    bump unity version

--- Assets/Scripts/VersionInfo.cs
+++ Assets/Scripts/VersionInfo.cs
@@ -4,8 +4,8 @@
 // License:         MIT License (http://www.opensource.org/licenses/mit-license.php)
 // Source Code:     https://github.com/Interkarma/daggerfall-unity
 // Original Author: Gavin Clayton (interkarma@dfworkshop.net)
-// Contributors:    
-// 
+// Contributors:
+//
 // Notes:
 //
 
@@ -20,5 +20,5 @@ public static class VersionInfo
     public const string DaggerfallUnityVersion = "1.1.1";
     public const string DaggerfallToolsForUnityVersion = "1.9.2";
 
-    public const string BaselineUnityVersion = "2019.4.40f1";
+    public const string BaselineUnityVersion = "2022.3.36f1";
 }
