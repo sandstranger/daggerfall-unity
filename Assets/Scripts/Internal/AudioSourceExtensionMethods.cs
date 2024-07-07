2c1ad3a62e8bdedf5ca74df3c3c9676c267ec38a
Vincent Wing <vincentwing00@gmail.com>
2024-07-06 17:21:31 -0700

    Probably fixed the repeated sounds issue when closing the character sheet, etc
    (cherry picked from commit 4efe8ebe831fb28e50d2aaa6b49341389c62506b)

--- Assets/Scripts/Internal/AudioSourceExtensionMethods.cs
+++ Assets/Scripts/Internal/AudioSourceExtensionMethods.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using DaggerfallWorkshop;
 
 namespace UnityEngine
@@ -8,6 +9,8 @@ namespace UnityEngine
     {
         private const float audioClipMaxDelay = 0.150f; //give up if sound takes longer to load
         private const float audioLoopMaxDelay = 2.0f; // don't care as much about loops accuracy
+
+        private static HashSet<AudioClip> waitingClips = new HashSet<AudioClip>();
 
         public static void PlayWhenReady(this AudioSource audioSource, AudioClip audioClip, float volumeScale)
         {
@@ -38,15 +41,23 @@ namespace UnityEngine
 
         private static IEnumerator WhenClipReadyCoroutine(AudioClip audioClip, float maxDelay, Action<AudioClip> clipHandler)
         {
+            if (waitingClips.Contains(audioClip))
+                yield break;
+            waitingClips.Add(audioClip);
             float loadWaitTimer = 0f;
             while (audioClip.loadState == AudioDataLoadState.Unloaded ||
                    audioClip.loadState == AudioDataLoadState.Loading)
             {
                 loadWaitTimer += Time.unscaledDeltaTime;
                 if (loadWaitTimer > maxDelay)
+                {
+                    waitingClips.Remove(audioClip);
                     yield break;
+
+                }
                 yield return null;
             }
+            waitingClips.Remove(audioClip);
             clipHandler(audioClip);
         }
     }
